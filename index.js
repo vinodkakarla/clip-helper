8de0c2bce2e3fec5f691df7f7abe8a7cb04e5773
vinodk <vinod.kakarla@imaginea.com>
2016-07-15 12:59:32 +0530

    Added multiple features and fixed minor defects
    * Add Help Content to the app
    * Enabled shortcut configurations
    * Included feature for preserving 10 recently used clips

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-var app = require('app')
-var BrowserWindow = require('browser-window')
-const globalShortcut = require('global-shortcut');
+var electron = require('electron');
+var jetpack = require('fs-jetpack');
+var app = electron.app;
+var BrowserWindow = electron.BrowserWindow;
+const globalShortcut = electron.globalShortcut;
 
+console.log(require.resolve('electron'));
 // var gui = require('nw.gui');
 // var win = gui.Window.get();
+
+var homeDirectory = process.env.HOME || process.env.USERPROFILE;
+//return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME']
+var filePath = homeDirectory + "/.clipManager";
+var shortcutKey = 'e';
+var acceptedKeys = "e|g|k|l|j"
+if (jetpack.exists(filePath)) {
+  var valueSet = jetpack.read(filePath, 'json');
+  var key = valueSet.shortcutKey;
+  if (key && key.length == 1 && key.match(acceptedKeys)) {
+    shortcutKey = key;
+  }
+}
+
 
 app.on('ready', function() {
   console.log("App getting started..");
@@ -15,36 +32,37 @@ app.on('ready', function() {
     frame: false,
     // kiosk: false
   })
-  // console.log("App getting started..1");
+  console.log("App getting started..1");
   // console.log('file://' + __dirname + '/index.html');
 
-  mainWindow.setAlwaysOnTop(true);
+  //mainWindow.setAlwaysOnTop(true);
+  console.log('file://' + __dirname + '/index.html');
   // console.log('file://' + __dirname + '/index.html');
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
-  // console.log("App getting started..2");
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  console.log("App getting started..2");
   mainWindow.on('blur', function() {
     // console.log('I do not want to be closed');
     mainWindow.blurWebView();
-    mainWindow.hide();
+    //mainWindow.hide();
     //mainWindow.close();
   });
-  //mainWindow.openDevTools();
+  mainWindow.openDevTools();
   // console.log(globalShortcut);
-  globalShortcut.unregister('ctrl+e');
+  globalShortcut.unregister('ctrl+' + shortcutKey);
   // Register Global shortcut
-  // console.log('registering');
-  var ret = globalShortcut.register('ctrl+e', function() {
+  console.log('registering');
+  var ret = globalShortcut.register('ctrl+' + shortcutKey, function() {
     mainWindow.show();
     mainWindow.focus();
     // win.show();
     // win.focus();
-    console.log('ctrl+e is pressed');
+    console.log('ctrl+' + shortcutKey + ' is pressed');
   });
   if (!ret) {
     console.log('Keyboard shortcut registration failed');
   }
   // Check whether a shortcut is registered.
-  console.log(globalShortcut.isRegistered('ctrl+e'));
+  console.log(globalShortcut.isRegistered('ctrl+' + shortcutKey));
 })
 
 // app.on('browser-window-blur', function() {
@@ -63,8 +81,8 @@ app.on('ready', function() {
 // })
 
 app.on("window-all-closed", function() {
-  console.log(globalShortcut.isRegistered('ctrl+e') + " *** ");
-  globalShortcut.unregister('ctrl+e');
+  console.log(globalShortcut.isRegistered('ctrl+' + shortcutKey) + " *** ");
+  globalShortcut.unregister('ctrl+' + shortcutKey);
   globalShortcut.unregisterAll();
   app.quit();
 });
