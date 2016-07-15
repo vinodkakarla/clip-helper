b487b46496e1a6fd01558f413049bff2252bfc12
vinodk <vinod.kakarla@imaginea.com>
2016-07-15 14:53:46 +0530

    Changes made for this app's title

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ var BrowserWindow = electron.BrowserWindow;
 const globalShortcut = electron.globalShortcut;
 
 console.log(require.resolve('electron'));
-// var gui = require('nw.gui');
-// var win = gui.Window.get();
 
 var homeDirectory = process.env.HOME || process.env.USERPROFILE;
 //return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME']
@@ -21,32 +19,31 @@ if (jetpack.exists(filePath)) {
   }
 }
 
-
 app.on('ready', function() {
   console.log("App getting started..");
   var mainWindow = new BrowserWindow({
     width: 800,
     height: 500,
     transparent: true,
+    focusable: true,
+    title: "Clip Manager",
     // alwaysontop: false,
     frame: false,
     // kiosk: false
   })
   console.log("App getting started..1");
-  // console.log('file://' + __dirname + '/index.html');
 
-  //mainWindow.setAlwaysOnTop(true);
+  mainWindow.setAlwaysOnTop(true);
   console.log('file://' + __dirname + '/index.html');
-  // console.log('file://' + __dirname + '/index.html');
   mainWindow.loadURL('file://' + __dirname + '/index.html');
   console.log("App getting started..2");
   mainWindow.on('blur', function() {
     // console.log('I do not want to be closed');
     mainWindow.blurWebView();
-    //mainWindow.hide();
+    mainWindow.hide();
     //mainWindow.close();
   });
-  mainWindow.openDevTools();
+  //mainWindow.openDevTools();
   // console.log(globalShortcut);
   globalShortcut.unregister('ctrl+' + shortcutKey);
   // Register Global shortcut
@@ -54,8 +51,6 @@ app.on('ready', function() {
   var ret = globalShortcut.register('ctrl+' + shortcutKey, function() {
     mainWindow.show();
     mainWindow.focus();
-    // win.show();
-    // win.focus();
     console.log('ctrl+' + shortcutKey + ' is pressed');
   });
   if (!ret) {
@@ -64,21 +59,6 @@ app.on('ready', function() {
   // Check whether a shortcut is registered.
   console.log(globalShortcut.isRegistered('ctrl+' + shortcutKey));
 })
-
-// app.on('browser-window-blur', function() {
-//   console.log('I do not want to be closed1');
-//
-// })
-
-// app.on('app-command', function(e, cmd) {
-//   // Navigate the window back when the user hits their mouse back button
-//   if (cmd === 'browser-backward' && app.webContents.canGoBack()) {
-//     app.webContents.goBack();
-//   }
-//   console.log(cmd);
-//   console.log(e);
-//   console.log(app.webContents.canGoBack());
-// })
 
 app.on("window-all-closed", function() {
   console.log(globalShortcut.isRegistered('ctrl+' + shortcutKey) + " *** ");
